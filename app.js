0eda3a9ed302a9fa1e05a2cb86f079bbbefd52f1
ryanschoolbs <ryanschoolbs@gmail.com>
2021-12-01 01:16:31 +0800

    add form validation

--- app.js
+++ app.js
@@ -1,1 +1,62 @@
+const form = document.querySelector("form");
+const firstName = document.getElementById("first-name");
+const lastName = document.getElementById("last-name");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
 
+form.addEventListener("submit", (e) => {
+   e.preventDefault();
+
+   checkInputs();
+});
+
+function checkInputs() {
+   const firstNameValue = firstName.value.trim();
+   const lastNameValue = lastName.value.trim();
+   const emailValue = email.value.trim();
+   const passwordValue = password.value.trim();
+
+   if (firstNameValue === "") {
+      setErrorFor(firstName, "First Name cannot be empty");
+   } else {
+      setSuccessFor(firstName);
+   }
+
+   if (lastNameValue === "") {
+      setErrorFor(lastName, "Last Name cannot be empty");
+   } else {
+      setSuccessFor(lastName);
+   }
+
+   if (emailValue === "") {
+      setErrorFor(email, "Email cannot be blank");
+   } else if (!isEmail(emailValue)) {
+      setErrorFor(email, "Not a valid email");
+   } else {
+      setSuccessFor(email);
+   }
+
+   if (passwordValue === "") {
+      setErrorFor(password, "Password cannot be blank");
+   } else {
+      setSuccessFor(password);
+   }
+}
+
+function setErrorFor(input, message) {
+   const formControl = input.parentElement;
+   const small = formControl.querySelector("small");
+   formControl.className = "form-control error";
+   small.innerText = message;
+}
+
+function setSuccessFor(input) {
+   const formControl = input.parentElement;
+   formControl.className = "form-control success";
+}
+
+function isEmail(email) {
+   return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      email
+   );
+}
